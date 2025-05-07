85bda7e398838f3c26ba61371e45c13b7a0414ed
郝多鱼 <haoduoyu@haoduoyudeMacBook-Air.local>
2025-05-07 21:18:18 +0800

    Signing in and signing up

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const db = require('./db');
 const models = require('./models');
@@ -12,13 +13,28 @@ const port = process.env.PORT || 4000;
 
 const app = express();
 db.connect(DB_HOST);
+
+const getUser = token => {
+    if(token) {
+        try {
+            return jwt.verify(token, process.env.JWT_SECRET);
+        } catch (err) {
+            throw new Error('Session invalid');
+        }
+    }
+};
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: () => {
-        return { models };
+    context: ({req}) => {
+        const token = req.headers.authorization;
+        const user = getUser(token);
+        console.log(user);
+        return { models, user };
     }
 });
+
 server.start().then(() => {
     server.applyMiddleware({ app, path: '/api' });
     app.listen({ port }, () =>
